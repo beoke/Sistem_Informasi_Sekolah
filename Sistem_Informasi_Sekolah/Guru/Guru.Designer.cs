bb4d10758d5ea786505cc7db046afc2c338f7510
Dhafa <sucsespart1@gmail.com>
2024-09-18 14:28:43 +0700

    design awal

---
+++ Sistem_Informasi_Sekolah/Guru/Guru.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Sistem_Informasi_Sekolah.Guru
+{
+    partial class Guru
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            radioButton4 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            dataGridView2 = new DataGridView();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.Controls.Add(radioButton4);
+            panel1.Controls.Add(radioButton3);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Controls.Add(textBox7);
+            panel1.Controls.Add(textBox6);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(dataGridView2);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(dataGridView1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(745, 509);
+            panel1.TabIndex = 1;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(431, 179);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(37, 19);
+            radioButton4.TabIndex = 23;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "S3";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(376, 179);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(37, 19);
+            radioButton3.TabIndex = 22;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "S2";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(337, 179);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(37, 19);
+            radioButton2.TabIndex = 21;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "S1";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(292, 179);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(39, 19);
+            radioButton1.TabIndex = 20;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "D3";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(517, 273);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(203, 23);
+            textBox7.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(517, 229);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(203, 23);
+            textBox6.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(292, 273);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(203, 23);
+            textBox5.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(292, 229);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(203, 23);
+            textBox4.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(292, 135);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(203, 23);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(292, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(203, 23);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(292, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(203, 23);
+            textBox1.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(527, 17);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(193, 159);
+            pictureBox1.TabIndex = 12;
+            pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(564, 476);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 11;
+            button3.Text = "Save";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(645, 476);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 11;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(473, 476);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 10;
+            button1.Text = "New";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(275, 313);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.Size = new Size(445, 157);
+            dataGridView2.TabIndex = 9;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(517, 255);
+            label8.Name = "label8";
+            label8.Size = new Size(31, 15);
+            label8.TabIndex = 8;
+            label8.Text = "Kota";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(517, 211);
+            label7.Name = "label7";
+            label7.Size = new Size(104, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Intansi Pendidikan";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(292, 255);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Tahun Lulus";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(292, 211);
+            label5.Name = "label5";
+            label5.Size = new Size(109, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Jurusan Pendidikan";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(292, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(108, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Tingkat Pendidikan";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(292, 117);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Tgl Lahir";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(292, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nama";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(292, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Guru Id";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(16, 13);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(240, 486);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Guru
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(764, 523);
+            Controls.Add(panel1);
+            Name = "Guru";
+            Text = "Guru";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView2;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+    }
+}
---
+++ Sistem_Informasi_Sekolah/Guru/Guru.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistem_Informasi_Sekolah.Guru
+{
+    public partial class Guru : Form
+    {
+        public Guru()
+        {
+            InitializeComponent();
+        }
+
+    
+    }
+}
