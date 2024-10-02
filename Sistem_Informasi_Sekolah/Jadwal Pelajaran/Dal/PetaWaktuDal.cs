ec3f45866e1cc4bce73cffb2f85b8ed37f86502a
Dhafa <sucsespart1@gmail.com>
2024-10-02 23:13:14 +0700

    add dall insert

--- Sistem_Informasi_Sekolah/Jadwal Pelajaran/Dal/PetaWaktuDal.cs
+++ Sistem_Informasi_Sekolah/Jadwal Pelajaran/Dal/PetaWaktuDal.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Dapper;
+using Sistem_Informasi_Sekolah.DataIndukSiswa.Helpers;
+using Sistem_Informasi_Sekolah.Jadwal_Pelajaran.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sistem_Informasi_Sekolah.Jadwal_Pelajaran.Dal
 {
-    internal class PetaWaktuDal
+    public class PetaWaktuDal
     {
+        public int Insert (PetaWaktuModel timeslot)
+        {
+            const string sql = @"
+            INSERT INTO TimeslotMapel(
+                KelasId, Hari, JenisJadwal, JamMulai, Jam selesai 
+                MapelId, GuruId, Keterangan)
+            OUTPUT INSERTED.TimeslotMapel
+            VALUES( 
+                @KelasId, @Hari, @JenisMapel, @JamMulai, @JamSelesai,
+                @MapelId, @GuruId, @Keterangan)";
+
+            var dp = new DynamicParameters();
+            dp.Add("@KelasId", timeslot.KelasId, DbType.Int16);
+            dp.Add("@Hari",timeslot.Hari, DbType.String);
+            dp.Add("@JenisJadwal", timeslot.JenisJadwal, DbType.String);
+            dp.Add("@JamMulai", timeslot.JamMulai, DbType.String);
+            dp.Add("@JamSelesai", timeslot.JamSelesai, DbType.String);
+            dp.Add("@MapelId", timeslot.MapelId, DbType.Int16);
+            dp.Add("@GuruId", timeslot.GuruId, DbType.Int16);
+            dp.Add("@Keterangan", timeslot.Keterangan, DbType.String);
+
+            using var con = new SqlConnection(ConnStringHelper.Get());
+            var result = con.QuerySingle<int>(sql, dp);
+            return result;
+        }
+
+        public void Update (PetaWaktuModel timeslot)
+        {
+
+        }
     }
 }
